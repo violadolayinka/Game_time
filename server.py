26d0b3107fc1139162a48aee3f79a5366d21f80c
violadolayinka <volayinka@gmail.com>
2015-11-19 14:40:13 -0800

    Completed the MVP for my Twilio App

--- server.py
+++ server.py
@@ -1,52 +1,35 @@
-from bootcamp import StudentApp
-from flask import request
+from flask import Flask, request
 import json
 import requests
+from twilio.rest import TwilioRestClient
+# ACCOUNT_SID = "AC08104111c524d56d95117df1d60f2283"
+# ACCOUNT_TOKEN = "d5429aa8bd9592b9fd94c0a3b8a9877c"
 
-ACCOUNT_SID = "AC08104111c524d56d95117df1d60f2283"
-ACCOUNT_TOKEN = "d5429aa8bd9592b9fd94c0a3b8a9877c"
+account_sid = "AC08104111c524d56d95117df1d60f2283"
+auth_token  = "d5429aa8bd9592b9fd94c0a3b8a9877c"
+client = TwilioRestClient(account_sid, auth_token)
+fromnumber = "+18563228074"
+app = Flask(__name__)
 
-app = StudentApp("Viola")
 
-cities = {
-    "San Francisco":"60 and cloudy",
-    "Houston":"80 and muggy",
-    "Cleveland":"32 and snowing",
-}
-
-@app.route('/weather')
-def weather():
-    city = request.values.get('city', 'San Francisco')
-    url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=2de143494c0b295cca9337e1e96b00e0"
+@app.route('/countryfacts')
+def country_fact():
+    text = request.values.get('Body', 'United States of America')
+    # text = text.strip()
+    number = request.values.get('From', '+12672502802')
+    url = "https://restcountries.eu/rest/v1/all"
     response = requests.get(url)
-    json = response.json()
-    tempK = json.get("main").get("temp")
-    return "The weather in {city} is {weather} Kelvin".format(city=city, weather=tempK)
-
-    # weather = cities.get(city, "[WEATHER DATA UNAVAILABLE]")
-    # return "The weather in {city} is {weather}".format(city=city, weather=weather)
-
-@app.route('/business_phone')
-def business_phone():
-    if app.available:
-        return """<?xml version="1.0" encoding="UTF-8"?>
-        <Response>
-          <Dial>+12672502802</Dial>
-        </Response>"""
-    else:
-        return """<?xml version="1.0" encoding="UTF-8"?>
-        <Response>
-          <Say>Viola is unavailable</Say>
-          <Record action = "/hangup" />
-        </Response>"""
-
-@app.route('/recordings')
-def show_recordings():
-    api = "https://api.twilio.com/2010-04-01"
-    uri = "/Accounts/" + ACCOUNT_SID + "/Recordings.json"
-    credentials = (ACCOUNT_SID,ACCOUNT_TOKEN)
-    response = requests.get(api + uri, auth=credentials)
-    recordings = json.loads(response.content)
-    return app.display_recordings(recordings)
+    #a list of countries
+    countries = response.json()
+    #find a county
+    for country in countries:
+        name = country.get("name")
+        if text == name:
+            population = country.get("population")
+            message = client.messages.create(body="The population in {name} is {population}".format(name=name,
+                                                                   population=population),to=number,from_=fromnumber)
+            return ""
+    message = client.messages.create(body="Not found",to=number,from_=fromnumber)
+    return ""
 
 app.run()
